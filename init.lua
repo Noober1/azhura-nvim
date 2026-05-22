556358a10b904f4331385a9a4ad782c39f1bdd6f
noober1 <cucu.ruhiyatna3@gmail.com>
2026-05-22 21:01:35 +0900

    Initial nvim config with lazy.nvim, LSP, Copilot, and custom IJKL keymaps

---
+++ init.lua
@@ -0,0 +1,254 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Basic options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+
+-- Plugins
+require("lazy").setup({
+  -- Colorscheme
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight-night")
+    end,
+  },
+
+  -- File explorer
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      keymaps = {
+        ["i"] = "actions.parent",
+        ["k"] = "actions.select",
+      },
+      view_options = { show_hidden = true },
+    },
+  },
+
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Buffers" },
+    },
+  },
+
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = { theme = "tokyonight" },
+  },
+
+  -- LSP & Completion
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      require("config.lsp")
+    end,
+  },
+
+  -- Treesitter (syntax highlighting)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+
+  -- Indent guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      indent = { char = "│" },
+      scope = { enabled = true },
+    },
+  },
+
+  -- Session management
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("persistence").setup()
+    end,
+  },
+
+  -- Diagnostics UI
+  {
+    "folke/trouble.nvim",
+    keys = {
+      { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
+    },
+  },
+
+  -- Emmet
+  {
+    "mattn/emmet-vim",
+    ft = { "html", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    init = function()
+      vim.g.user_emmet_leader_key = "<C-e>"
+      vim.g.user_emmet_settings = {
+        javascriptreact = { extends = "jsx" },
+        typescriptreact = { extends = "jsx" },
+      }
+    end,
+  },
+
+  -- AI code completion
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+
+  -- Prettier formatter
+  {
+    "stevearc/conform.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          javascript = { "prettier" },
+          typescript = { "prettier" },
+          javascriptreact = { "prettier" },
+          typescriptreact = { "prettier" },
+          css = { "prettier" },
+          scss = { "prettier" },
+        },
+      })
+    end,
+  },
+}, {
+  rocks = { enabled = false },
+})
+
+-- ============================================================
+-- KEYMAPS
+-- ============================================================
+
+local map = function(modes, lhs, rhs, desc)
+  vim.keymap.set(modes, lhs, rhs, { noremap = true, silent = true, desc = desc })
+end
+
+-- Insert mode pindah ke 'a' (karena 'i' dipakai navigasi)
+map("n", "a",     "i",      "Insert before cursor")
+map("n", "A",     "I",      "Insert beginning of line")
+map("n", "<C-a>", "a",      "Append after cursor")  -- ganti append
+
+-- IJKL navigation (normal + visual)
+map({"n","v"}, "i", "k",    "Up")
+map({"n","v"}, "k", "j",    "Down")
+map({"n","v"}, "j", "h",    "Left")
+map({"n","v"}, "l", "l",    "Right")
+
+-- Word jump (sesuaikan dengan posisi IJKL)
+map({"n","v"}, "I", "5k",   "Jump up 5 lines")
+map({"n","v"}, "K", "5j",   "Jump down 5 lines")
+map({"n","v"}, "J", "b",    "Word backward")
+map({"n","v"}, "L", "w",    "Word forward")
+
+-- Scroll (hindari <C-i> karena = <Tab> di terminal)
+map("n", "<C-u>", "<C-u>zz", "Scroll up half page")
+map("n", "<C-d>", "<C-d>zz", "Scroll down half page")
+
+-- Kembali ke normal mode dengan double i
+vim.keymap.set("i", "ii", "<Esc>", { noremap = true, silent = true })
+
+-- Undo/redo tetap
+map("n", "u",     "u",       "Undo")
+map("n", "U",     "<C-r>",   "Redo")
+
+-- File explorer
+map("n", "-", "<cmd>Oil<cr>", "Open file explorer")
+
+-- Window navigation
+map("n", "<leader>wj", "<C-w>h", "Window left")
+map("n", "<leader>wl", "<C-w>l", "Window right")
+map("n", "<leader>wi", "<C-w>k", "Window up")
+map("n", "<leader>wk", "<C-w>j", "Window down")
+
+-- Claude Code terminal
+map("n", "<leader>ct", ":terminal claude-code<CR>", "Open Claude Code terminal")
+
+-- Tab navigation by number
+for i = 1, 9 do
+  map("n", "<leader>" .. i, i .. "gt", "Go to tab " .. i)
+end
+
+-- Session
+map("n", "<leader>qs", function() require("persistence").load() end, "Restore session")
+map("n", "<leader>ql", function() require("persistence").load({ last = true }) end, "Restore last session")
+map("n", "<leader>qd", function() require("persistence").stop() end, "Stop session save")
+
+-- Auto-format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.js", "*.ts", "*.jsx", "*.tsx", "*.css", "*.scss" },
+  callback = function()
+    require("conform").format({ bufnr = vim.api.nvim_get_current_buf() })
+  end,
+})
+
+-- Treesitter setup (after loading)
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local ok, configs = pcall(require, "nvim-treesitter.configs")
+    if ok then
+      configs.setup({
+        ensure_installed = { "tsx", "typescript", "javascript", "css", "scss", "json", "markdown", "lua", "vim" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  end,
+})
---
+++ lua/config/lsp.lua
@@ -0,0 +1,148 @@
+-- Mason setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "ts_ls",         -- TypeScript/JavaScript
+    "cssls",         -- CSS/SCSS
+    "jsonls",        -- JSON
+    "marksman",      -- Markdown
+    "lua_ls",        -- Lua
+    "tailwindcss",   -- Tailwind CSS
+    "emmet_ls",      -- Emmet
+  },
+  automatic_installation = true,
+})
+
+-- LSP capabilities
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+-- LSP Keymaps
+local function on_attach(client, bufnr)
+  local map = function(mode, lhs, rhs, desc)
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true, desc = desc })
+  end
+
+  map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+  map("n", "gr", vim.lsp.buf.references, "Find references")
+  map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+  map("n", "K", vim.lsp.buf.hover, "Hover")
+  map("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
+  map("n", "<leader>ca", vim.lsp.buf.code_action, "Code action")
+  map("n", "<leader>d", vim.diagnostic.open_float, "Show diagnostic")
+end
+
+-- Configure servers
+vim.lsp.config("ts_ls", {
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+vim.lsp.config("cssls", {
+  cmd = { "css-languageserver", "--stdio" },
+  filetypes = { "css", "scss", "less" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+vim.lsp.config("jsonls", {
+  cmd = { "vscode-json-languageserver", "--stdio" },
+  filetypes = { "json", "jsonc" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+vim.lsp.config("marksman", {
+  cmd = { "marksman", "server" },
+  filetypes = { "markdown" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+vim.lsp.config("lua_ls", {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+    },
+  },
+})
+
+vim.lsp.config("tailwindcss", {
+  cmd = { "tailwindcss-language-server", "--stdio" },
+  filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+vim.lsp.config("emmet_ls", {
+  cmd = { "emmet-language-server", "--stdio" },
+  filetypes = { "html", "css", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+  init_options = {
+    jsx = { options = { ["markup.attributes"] = { ["class"] = "className" } } },
+  },
+})
+
+-- Enable all configured servers
+vim.lsp.enable({ "ts_ls", "cssls", "jsonls", "marksman", "lua_ls", "tailwindcss", "emmet_ls" })
+
+-- Completion setup
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "copilot" },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
+})
+
+-- Diagnostics
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+})
